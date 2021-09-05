aa401932d8126e9f498ce2b81dd5bf05ddbea47f
Newiee <90134934+Newiee@users.noreply.github.com>
2021-09-05 13:40:16 +0300

    Add files via upload

---
+++ TwentyOne.py
@@ -0,0 +1,13 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+
+class TwentyOneApp(MDApp):
+    def build(self):
+        return MDLabel(text="Hello, World", halign="center")
+
+
+TwentyOneApp().run()
+
+
+
